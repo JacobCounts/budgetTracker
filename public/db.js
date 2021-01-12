8af2d2a399f34145c62bb78c5e30a92738912b52
Jacob Counts <infamous213@gmail.com>
2021-01-11 18:56:21 -0700

    added db/service worker

---
+++ public/db.js
@@ -0,0 +1,52 @@
+const { response } = require("express");
+const { get } = require("http");
+
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDataBase();
+    }
+};
+
+function saveRecord (record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDataBase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST", 
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/jason",
+                }
+            })
+            .then((response) => response.JSON())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+    saveRecord();
+}
+window.addEventListener("online", checkDataBase);
